6b6b81560fc60572124cf83fe69a3e5eb9b012b5
cimiox <cimiox@outlook.com>
2017-05-18 13:09:12 +0300

    first commit

---
+++ Assets/Scripts/Builder System/ZoomSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomSystem : MonoBehaviour
+{
+    public static void Zoom(GameObject go)
+    {
+        Camera.main.orthographicSize = (go.GetComponent<Renderer>().bounds.size.x / Camera.main.aspect) / 3;
+        Camera.main.transform.position = new Vector3(go.transform.position.x, go.transform.position.x, -10);
+        //
+    }
+}
---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ClickManager : MonoBehaviour, IPointerClickHandler
+{
+    public Text clicks;
+    public Slider experience;
+
+    public float Count { get; set; }
+    public int Level { get; set; }
+
+    private static readonly float EXP = 150;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        clicks.text = string.Format("Clicks: {0}\nLevel: {1}", ++Count, Level);
+
+        experience.maxValue = GetExp(Level);
+        experience.value = Count;
+
+        if (Count >= GetExp(Level))
+        {
+            Count = 0;
+            ++Level;
+        }
+    }
+
+    private void Init()
+    {
+        Level = 1;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private static float GetExp(float level)
+    {
+        if (level <= 1)
+            return EXP;
+
+        return EXP * level + GetExp(level - 1);
+    }
+}
