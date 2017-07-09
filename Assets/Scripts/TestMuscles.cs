61343eb1864bbcd8417830fe7232a55cb15806e3
cimiox <cimiox@outlook.com>
2017-07-09 17:09:01 +0300

    Player 1-5 lvl

---
+++ Assets/Scripts/TestMuscles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestMuscles : MonoBehaviour 
+{
+
+	public void OnValueChangedToggle(GameObject muscle)
+	{
+		if (this.GetComponent<Toggle>().isOn)
+			muscle.SetActive(true);
+		else
+			muscle.SetActive(false);
+	}
+}
