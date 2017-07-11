d6cf288f357459d0bea80ac51dbb7777ebe97386
cimiox <cimiox@outlook.com>
2017-07-11 23:39:46 +0300

    Muscle system changes
    Add Muscle experience
    Add levelup

--- Assets/Scripts/Builder System/FrontMuscles/LegsMuscles.cs
+++ Assets/Scripts/Builder System/FrontMuscles/LegsMuscles.cs
@@ -20,26 +20,66 @@ public class LegsMuscles : Muscle
 			localClicks = value;
 		}
 	}
-	public override bool isZoom { get; set; }
+	public override bool IsZoom { get; set; }
+    public int MuscleLevel;
 
     public override event ChangingClicks ChangeClicks;
+
+    private void Awake()
+    {
+        //TODO: Block Many calles
+        foreach (var item in SetMusclesInList(MuscleLevel, gameObject.transform.parent.transform.parent.gameObject))
+        {
+            Muscles.Add(item.Key, item.Value);
+        }
+    }
 
     protected override void Initialize()
     {
         ZoomSystem.Zoom(ZoomableGO);
-        isZoom = true;
+        IsZoom = true;
+        Multiplier = 1;
     }
 
     protected override void OnMouseDown()
     {
         ZoomableGO = gameObject;
 
-        if (!isZoom)
+        if (!IsZoom)
         {
             Initialize();
             return;
         }
 
         LocalClicks += Convert.ToInt32(1 * Multiplier);
+
+        if (localClicks >= GetMuscleExperience(MuscleLevel))
+            MuscleLevelUp(MuscleLevel, gameObject.transform.parent.transform.parent.gameObject);
+    }
+
+    protected override void MuscleLevelUp(int muscleLevel, GameObject parent)
+    {
+        foreach(var item in parent.GetComponentsInChildren<LegsMuscles>())
+        {
+            if (item.MuscleLevel == ++muscleLevel)
+            {
+                item.gameObject.SetActive(true);
+                break;
+            }
+        }
+
+        gameObject.SetActive(false);
+    }
+
+    protected override Dictionary<GameObject, Muscle> SetMusclesInList(int muscleLevel, GameObject parent)
+    {
+        var muscles = new Dictionary<GameObject, Muscle>();
+
+        foreach (var item in parent.GetComponentsInChildren<PressMuscle>())
+        {
+            muscles.Add(item.gameObject, item);
+        }
+
+        return muscles;
     }
 }
--- Assets/Scripts/Builder System/Muscle.cs
+++ Assets/Scripts/Builder System/Muscle.cs
@@ -10,9 +10,19 @@ public abstract class Muscle : MonoBehaviour
     public static GameObject ZoomableGO { get; set; }
     public static float Multiplier { get; set; }
     public abstract float LocalClicks { get; set; }
-    public abstract bool isZoom { get; set; }
+    public abstract bool IsZoom { get; set; }
+    public static Dictionary<GameObject, Muscle> Muscles { get; set; }
+
+    private int[] MuscleExperience = new int[10] 
+    {10, 5000, 11000, 17000, 23000, 29000, 35000, 43000, 47000, 55000};
 
 	protected abstract void OnMouseDown();
     protected abstract void Initialize();
-    
+    protected abstract void MuscleLevelUp(int muscleLevel, GameObject parent);
+    protected abstract Dictionary<GameObject, Muscle> SetMusclesInList(int muscleLevel, GameObject parent);
+
+    protected int GetMuscleExperience(int level)
+    {
+        return MuscleExperience[level - 1];
+    }
 }
