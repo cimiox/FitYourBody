64d82e5f13315ee5e049538f81dfd3ecef15b3c3
cimiox <cimiox@outlook.com>
2017-07-22 19:30:00 +0300

    Back Player
    build back player 1- 5 lvl - ok
    scripts for back muscles - ok

---
+++ Assets/Scripts/Builder System/BackMuscles/BackLegsMuscle.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackLegsMuscle : Muscle
+{
+    public override event ChangingClicks ChangeClicks;
+
+    private float localClicks;
+    public override float LocalClicks
+    {
+        get
+        {
+            return localClicks;
+        }
+        set
+        {
+            if (ChangeClicks != null)
+                ChangeClicks(value - localClicks);
+
+            localClicks = value;
+        }
+    }
+    public override bool IsZoom { get; set; }
+    public override int MuscleLevel { get; set; } = 1;
+    public static bool IsCalled { get; set; }
+
+    private void Awake()
+    {
+        if (!IsCalled)
+        {
+            AddMuscles(SetMusclesInList(gameObject.transform.parent.transform.parent.gameObject));
+
+            IsCalled = true;
+        }
+    }
+
+    protected override void Initialize()
+    {
+        ZoomSystem.Zoom(ZoomableGO);
+        IsZoom = true;
+        Multiplier = 1;
+    }
+
+    protected override void OnMouseDown()
+    {
+        ZoomableGO = gameObject;
+
+        if (!IsZoom)
+        {
+            Initialize();
+            return;
+        }
+
+        LocalClicks += Convert.ToInt32(1 * Multiplier);
+
+        if (localClicks >= GetMuscleExperience(MuscleLevel))
+            MuscleLevelUp(MuscleLevel, Muscles);
+    }
+
+    protected override void MuscleLevelUp(int muscleLevel, List<MuscleItems> list)
+    {
+        foreach (var item in list)
+        {
+            if (item.Muscle is BackLegsMuscle)
+            {
+                if (item.Muscle.MuscleLevel == (muscleLevel + 1))
+                {
+                    item.MuscleGO.SetActive(true);
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    protected override List<MuscleItems> SetMusclesInList(GameObject parent)
+    {
+        var muscles = new List<MuscleItems>();
+
+        int muscleLevel = 0;
+
+        for (int i = 0; i < parent.GetComponentsInChildren<HandsMuscles>().Length; i++)
+        {
+            if ((i + 1) % 2 == 0)
+                parent.GetComponentsInChildren<HandsMuscles>()[i].MuscleLevel = parent.GetComponentsInChildren<HandsMuscles>()[i - 1].MuscleLevel;
+            else
+                parent.GetComponentsInChildren<HandsMuscles>()[i].MuscleLevel = ++muscleLevel;
+
+            muscles.Add(new MuscleItems(parent.GetComponentsInChildren<HandsMuscles>()[i].gameObject, parent.GetComponentsInChildren<HandsMuscles>()[i]));
+        }
+
+        return muscles;
+    }
+}
---
+++ Assets/Scripts/Builder System/BackMuscles/NeckMuscle.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeckMuscle : Muscle
+{
+    public override event ChangingClicks ChangeClicks;
+
+    private float localClicks;
+    public override float LocalClicks
+    {
+        get
+        {
+            return localClicks;
+        }
+        set
+        {
+            if (ChangeClicks != null)
+                ChangeClicks(value - localClicks);
+
+            localClicks = value;
+        }
+    }
+    public override bool IsZoom { get; set; }
+    public override int MuscleLevel { get; set; } = 1;
+    public static bool IsCalled { get; set; }
+
+    private void Awake()
+    {
+        if (!IsCalled)
+        {
+            AddMuscles(SetMusclesInList(gameObject.transform.parent.transform.parent.gameObject));
+
+            IsCalled = true;
+        }
+    }
+
+    protected override void Initialize()
+    {
+        ZoomSystem.Zoom(ZoomableGO);
+        IsZoom = true;
+        Multiplier = 1;
+    }
+
+    protected override void OnMouseDown()
+    {
+        ZoomableGO = gameObject;
+
+        if (!IsZoom)
+        {
+            Initialize();
+            return;
+        }
+
+        LocalClicks += Convert.ToInt32(1 * Multiplier);
+
+        if (localClicks >= GetMuscleExperience(MuscleLevel))
+            MuscleLevelUp(MuscleLevel, Muscles);
+    }
+
+    protected override void MuscleLevelUp(int muscleLevel, List<MuscleItems> list)
+    {
+        foreach (var item in list)
+        {
+            if (item.Muscle is NeckMuscle)
+            {
+                if (item.Muscle.MuscleLevel == (muscleLevel + 1))
+                {
+                    item.MuscleGO.SetActive(true);
+                    gameObject.SetActive(false);
+                    break;
+                }
+            }
+        }
+    }
+
+    protected override List<MuscleItems> SetMusclesInList(GameObject parent)
+    {
+        var muscles = new List<MuscleItems>();
+
+        int muscleLevel = 0;
+
+        foreach (var item in parent.GetComponentsInChildren<NeckMuscle>())
+        {
+            item.MuscleLevel = ++muscleLevel;
+            muscles.Add(new MuscleItems(item.gameObject, item));
+        }
+
+        return muscles;
+    }
+}
